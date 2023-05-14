cbdba36b39d233f90a12ca51f386eb3f1611838c
Zhengke <zhengke996@163.com>
2023-05-14 15:30:56 +0800

    错误页处理

--- src/router/modules/mobile-routes.ts
+++ src/router/modules/mobile-routes.ts
@@ -11,5 +11,16 @@ const routes: RouteRecordRaw[] = [
     name: 'pins',
     component: () => import('@/views/pins'),
   },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('@/views/404'),
+  },
+  // 404 页面处理
+  {
+    path: '/:catchAll(.*)',
+    name: 'error',
+    redirect: '/404',
+  },
 ]
 export default routes
--- src/router/modules/pc-routes.ts
+++ src/router/modules/pc-routes.ts
@@ -16,6 +16,17 @@ const routes: RouteRecordRaw[] = [
         name: 'pins',
         component: () => import('@/views/pins'),
       },
+      {
+        path: '/404',
+        name: '404',
+        component: () => import('@/views/404'),
+      },
+      // 404 页面处理
+      {
+        path: '/:catchAll(.*)',
+        name: 'error',
+        redirect: '/404',
+      },
     ],
   },
 ]
